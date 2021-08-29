1dbd661c4aa41d26f9d7e84e98a638cea63ec4c2
0x6775737461 <fabiogustavo@tuta.io>
2021-08-29 09:22:09 -0400

    refactoring

--- main.sh
+++ main.sh
@@ -8,82 +8,95 @@ do
 	data_arr[${i}]=$(sed -n "${i}p" ${1})
 done
 
-# make requests to ufgd news and parse this data
-# maintin two files to compare in each requests
+# make requests to ufgd news and parse this data,
+# maintain two files to compare in each requests
 get_json() {
-	news_json='/tmp/ufgd_news.json'
+	path_json='/tmp/ufgd_news.json'
 
-	if [[ -e /tmp/ufgd_news.json ]]; then
-		cp  /tmp/ufgd_news.json /tmp/ufgd_news_old.json
+	#testing if file exists
+	hash=0
+	if [[ -e $path_json ]]; then
+		cp  $path_json "${path_json/.json/}_news.json"
 
-		req=$(curl -s "${data_arr[1]}" -o /tmp/ufgd_news.json)
-		if [[ ${req} -ne 0 ]]; then
-			curl -s \
-			-X POST \ "${data_arr[2]}/bot${data_arr[4]}/sendMessage" \
-			-d "chat_id=${data_arr[5]}" \
-			-d "text=bug_ufgd_news"
-		fi
+		curl -L -s "${data_arr[1]}" -o $path_json
+		
+		new_file_hash=$(md5sum < $path_json)
 
-		new_file_hash=$(md5sum < /tmp/ufgd_news.json)
-		old_file_hash=$(md5sum < /tmp/ufgd_news_old.json)
+		#TODO: correct this replacement
+		old_file_hash=$(md5sum < "${path_json/.json/}_news.json")
 
+		#compare if the site changed
 		hash=1
 		if [[ ${new_file_hash::32} != ${old_file_hash::32} ]]; then
 			hash=0
 		fi
+
+		[[ $hash -eq 0 ]] && parse_data $hash
+
 	else
-		curl -s "${data_arr[1]}" -o /tmp/ufgd_news.json
-		hash=0
+		curl -L -s "${data_arr[1]}" -o $path_json
+		parse_data $hash
+
 	fi
 }
 
 	
-news() {
-	if get_json; then
-		news_title=$(jq '.Informes[0].titulo' ${news_json} | \
-								 sed 's/\"//g; s/ /\+/g')
+parse_data() {
+	if [[ $1 -eq 0 ]]; then
 
-		news_desc=$(jq '.Informes[0].descricao' ${news_json} | \
-								sed 's/\"//g; s/ /\+/g')
+		title=$(jq '.Informes[0].titulo' "$path_json" | \
+			sed 's/\./\\./g; s/\"//g; s/  / /g; s/ /\+/g; s/\-/\\-/g')
 
-		news_resp_sec=$(jq '.Informes[0].setorResponsavel' ${news_json} | \
-										sed 's/\"//g; s/ /\+/g')
+		desc=$(jq '.Informes[0].descricao' "$path_json" | \
+			sed 's/\./\\./g; s/\"//g; s/  / /g; s/ /\+/g; s/\-/\\-/g')
+
+		resp_sec=$(jq '.Informes[0].setorResponsavel' "$path_json" | \
+			sed 's/\./\\./g; s/\"//g; s/  / /g; s/ /\+/g; s/\-/\\-/g')
 										
-		news_url=$(jq '.Informes[0].url' ${news_json} | \
-							 sed 's/\"//g; s/ /\+/g')
+		url=$(jq '.Informes[0].url' "$path_json" | \
+			sed 's/\./\\./g; s/\"//g; s/  / /g; s/ /\+/g; s/\-/\\-/g')
 
-		news_changes_date=$(jq '.Informes[0].alteracao' ${news_json} | \
-												sed 's/\"//g; s/ /\+/g')
+		#formating to post in telegram
 
-		title="*${news_title//\-/\\-}* "
-		changes_date="_\[${news_changes_date}\]_%0A"
-		resp_sec="%0A__Fonte:+${news_resp_sec//\-/\\-}__%0A%0A"
-		desc="${news_desc//\./\\.}%0A"
-		ufgd_main_url="https://ufgd.edu.br"
-		url="\[[acesse\_aqui](${ufgd_main_url//\./\\.}${news_url//\-/\\-})\]"
+		title="*${title}*+"
+		resp_sec="%0A__Fonte:+${resp_sec}__%0A%0A"
+		desc="${desc}%0A"
+		ufgd_url="https://ufgd.edu.br"
+		url="\[[acesse\_aqui](${ufgd_url}${url})\]"
+	
+		full_text_news="${title}${resp_sec}${desc}${url}"
 
-		full_text_news="${title}${changes_date}${resp_sec}${desc}${url}"
+		bot_tg $1
 
-		if [[ ${hash} -eq 0 ]]; then
-			bot_tg
-		fi
+	else
+		echo "[($date +%H%M)] error: $1" >> error_log
+		bot_tg $1
+
 	fi
 }
 
 bot_tg() {
+	chat_id=${data_arr[4]}
+	text=${full_text_news}
 
-	curl -s\
+	# send message to de dev
+	if [[ $1 -eq 1 ]]; then
+		chat_id=${data_arr[5]}
+		text="bug+on+bot"
+	fi
+
+	curl -L -s \
 	-X POST \
 	"${data_arr[2]}/bot${data_arr[3]}/sendMessage" \
-	-d chat_id=${data_arr[4]} \
+	-d chat_id=$chat_id \
 	-d parse_mode='MarkdownV2' \
-	-d text="${full_text_news}"
+	-d text="$text"
 }
 
 main() {
 	while true; do
-		news
-		sleep 1800 
+		get_json
+		sleep 300 
 	done
 }
 
